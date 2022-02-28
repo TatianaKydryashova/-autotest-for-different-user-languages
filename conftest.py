75a86804e40205588a8434f43d37e0590ab98c16
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-02-28 14:43:01 +0300

    test for different user languages

--- conftest.py
+++ conftest.py
@@ -2,10 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-
-
-
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language")
